37e52b04f78a7675ef34b45be19ebce254f4267a
Timur Uzel <timurt.uzel@gmail.com>
2018-11-13 22:07:04 -0500

    unnecessary index

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-import App from './components/App/index'
+import App from './components/App'
 import * as serviceWorker from './config/serviceWorker'
 
 ReactDOM.render(<App />, document.getElementById('root'))
