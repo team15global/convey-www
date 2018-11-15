1a2487e7758b4913913f047cbf54c7a78347d136
Timur Uzel <timurt.uzel@gmail.com>
2018-11-14 21:28:21 -0500

    inputbox

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,28 +1,42 @@
 import React, { Component, Fragment } from 'react'
 import styled from 'styled-components'
+import TextField from '@material-ui/core/TextField'
 
-import Logo from './Logo'
 import Globals from './Globals'
+
+const colors = {
+  bg: '#fff',
+  text: '#fff',
+}
 
 const Wrapper = styled.header`
   text-align: center;
-  background-color: #282c34;
+  background-color: ${colors.bg};
   min-height: 100vh;
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  font-size: calc(10px + 2vmin);
-  color: white;
 `
 
-const Title = styled.p`
+const Title = styled.h1`
   font-family: Spicy Rice;
-  font-size: 64px;
+  font-size: 5rem;
+  margin-top: -2rem;
 `
 
-const Link = styled.a`
-  color: #61dafb;
+const Input = styled(TextField)`
+  width: 60%;
+
+  input:-webkit-autofill,
+  input:-webkit-autofill:hover,
+  input:-webkit-autofill:focus textarea:-webkit-autofill,
+  textarea:-webkit-autofill:hover textarea:-webkit-autofill:focus,
+  select:-webkit-autofill,
+  select:-webkit-autofill:hover,
+  select:-webkit-autofill:focus {
+    -webkit-box-shadow: 0 0 0px 1000px ${colors.bg} inset;
+  }
 `
 
 export default class extends Component {
@@ -30,11 +44,14 @@ export default class extends Component {
     return (
       <Fragment>
         <Wrapper>
-          <Logo />
           <Title>Convey</Title>
-          <Link href="https://reactjs.org" target="_blank">
-            Learn React
-          </Link>
+          <Input
+            label="Email Address"
+            margin="normal"
+            name="email"
+            type="email"
+            variant="outlined"
+          />
         </Wrapper>
         <Globals />
       </Fragment>
